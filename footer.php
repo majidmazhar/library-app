e85465d8bcc4901cb8bd7cdc88e547915affad1b
majidmazhar <majidmazhar2332@gmail.com>
2023-05-05 00:58:48 -0700

    1402/2/15_library

---
+++ footer.php
@@ -0,0 +1,12 @@
+
+</div>
+
+
+<script src="./assets/lib/bootstrap-5.2.3-dist/js/bootstrap.min.js"></script>
+    <script src="./assets/js/main.js"></script>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+
+</body>
+
+</html>
